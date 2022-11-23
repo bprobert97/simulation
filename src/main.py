cc6fd2661d4ecf05f2f8e623a7fa373ee5dbd0b9
Christopher Lowe <christopher.lowe@strath.ac.uk>
2022-11-23 17:00:40 +0000

    Schedule node exists (not doing anything)
    No functionality, just a continuous contact with the other nodes atm

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import itertools
 import random
+import sys
 from queue import PriorityQueue
 from copy import deepcopy
 import simpy
@@ -12,7 +13,7 @@ from scheduling import Scheduler
 from bundles import Buffer, Bundle
 
 
-SCHEDULER_ID = 999
+SCHEDULER_ID = 0
 SCHEDULER_BUFFER_CAPACITY = 1000
 NUM_NODES = 4
 NODE_BUFFER_CAPACITY = 100
@@ -30,15 +31,15 @@ def bundle_generator(env, sources, destinations):
 	while True:
 		yield env.timeout(random.expovariate(1 / BUNDLE_ARRIVAL_RATE))
 		source = random.choice(sources)
-		dests = [x for x in destinations if x != source.uid]
+		dests = [x for x in destinations if x.uid != source.uid]
 		destination = random.choice(dests)
 		size = random.randint(*BUNDLE_SIZE)
 		deadline = env.now + BUNDLE_TTL
-		print(f"bundle generated on node {source.uid} at time {env.now}")
+		print(f"bundle generated on node {source.uid} at time {env.now} for destination {destination.uid}")
 		source.buffer.append(
 			Bundle(
 				source.uid,
-				destination,
+				destination.uid,
 				size=size,
 				deadline=deadline
 			)
@@ -46,12 +47,12 @@ def bundle_generator(env, sources, destinations):
 
 
 def init_nodes(num_nodes, cp):
-	node_dict = {}
-	for n_uid in range(num_nodes):
+	node_list = []
+	for n_uid in range(1, num_nodes+1):
 		n = Node(
 			n_uid,
 			buffer=Buffer(NODE_BUFFER_CAPACITY),
-			outbound_queues={x: [] for x in range(num_nodes)},
+			outbound_queues={x: [] for x in range(1, num_nodes+1)},
 			contact_plan=cp
 		)
 
@@ -61,46 +62,33 @@ def init_nodes(num_nodes, cp):
 		# any delay incurred through travel (OWLT)
 		pub.subscribe(n.bundle_receive, str(n_uid) + "bundle")
 
-		# bundles = bundle_generator(
-		# 	NUM_BUNDLES,
-		# 	n_uid,
-		# 	[x for x in range(num_nodes) if x != n_uid]
-		# )
-		#
-		# for b in bundles:
-		# 	n.buffer.append(b)
+		node_list.append(n)
 
-		node_dict[n_uid] = n
-
-	# Add the scheduler node
-	# node_dict[SCHEDULER_ID] = Node(
-	# 	SCHEDULER_ID,
-	# 	buffer=Buffer(SCHEDULER_BUFFER_CAPACITY),
-	# 	contact_plan=cp,
-	# 	scheduler=Scheduler(contact_plan=cp)
-	# )
-
-	return {uid: n for uid, n in node_dict.items()}
+	return node_list
 
 
 def init_contact_plan():
 	return [
-		Contact(0, 1, 5, 10, owlt=1),
-		Contact(1, 0, 6, 12, owlt=1),
-		Contact(0, 2, 15, 20, owlt=1),
-		Contact(2, 0, 15, 21, owlt=1),
-		Contact(2, 3, 24, 26, owlt=1),
-		Contact(3, 2, 25, 26, owlt=1)
+		Contact(0, 1, 0, sys.maxsize),
+		Contact(0, 2, 0, sys.maxsize),
+		Contact(0, 3, 0, sys.maxsize),
+		Contact(0, 4, 0, sys.maxsize),
+		Contact(1, 2, 5, 10, owlt=1),
+		Contact(2, 1, 6, 12, owlt=1),
+		Contact(1, 3, 15, 20, owlt=1),
+		Contact(3, 1, 15, 21, owlt=1),
+		Contact(3, 4, 24, 26, owlt=1),
+		Contact(4, 3, 25, 26, owlt=1)
 	]
 
 
 def create_route_tables(nodes, cp):
-	for n_uid, node in nodes.items():
-		for other_uid, other in {
-			x_uid: x for x_uid, x in nodes.items() if x_uid != n_uid
-		}.items():
-			routes = cgr_yens(n_uid, other_uid, 0, 5, cp)
-			node.route_table[other_uid] = [] if not routes else routes
+	for node in nodes:
+		for other in [
+			x for x in nodes if x.uid != node.uid
+		]:
+			routes = cgr_yens(node.uid, other.uid, 0, 5, cp)
+			node.route_table[other.uid] = [] if not routes else routes
 
 
 if __name__ == "__main__":
@@ -114,11 +102,17 @@ if __name__ == "__main__":
 	"""
 	env = simpy.Environment()
 	cp = init_contact_plan()
+	scheduler = Node(
+		SCHEDULER_ID,
+		buffer=Buffer(SCHEDULER_BUFFER_CAPACITY),
+		contact_plan=cp,
+		scheduler=Scheduler(contact_plan=cp)
+	)
 	nodes = init_nodes(NUM_NODES, cp)
 	create_route_tables(nodes, cp)
 
 	env.process(bundle_generator(env, nodes, nodes))
-	for node in nodes.values():
+	for node in [scheduler] + nodes:
 		env.process(node.bundle_assignment_controller(env))
 		env.process(node.contact_controller(env))  # Generator that initiates contacts
 		# TODO Need to add in the generators that do the regular bundle assignment and
