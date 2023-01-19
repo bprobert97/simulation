184184323f8e2ad56239ad485de2fdfceb297c50
Christopher Lowe <christopher.lowe@strath.ac.uk>
2023-01-19 09:32:22 +0000

    Moved old naive results

--- src/plotResults.py
+++ src/plotResults.py
@@ -56,8 +56,7 @@ def plot_performance_metrics(schemes, uncertainties, congestions, metrics):
 	fig.legend(
 		ax[0, 0].lines,
 		# ["Naive", "First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
-		["Uncertainty = 0.7", "Uncertainty = 0.8", "Uncertainty = 0.9",
-		 "Uncertainty = 1.0"],
+		["Reliability = 0.7", "Reliability = 0.8", "Reliability = 0.9", "Reliability = 1.0"],
 		loc='upper center',
 		# ncol=len(schemes),
 		ncol=len(uncertainties)
@@ -95,7 +94,7 @@ def plot_first_pickups(schemes, first_pickups, congestions):
 	# Add a legend at the top of the figure
 	fig.legend(
 		ax.lines,
-		["First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
+		["Naive", "First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
 		# ["Uncertainty = 0.7", "Uncertainty = 0.8", "Uncertainty = 0.9",
 		#  "Uncertainty = 1.0"],
 		loc='upper center',
@@ -109,24 +108,24 @@ def plot_first_pickups(schemes, first_pickups, congestions):
 	plt.show()
 
 
-# filename_base = "results//nominal//results"
-filename_base = "results//uncertainty//results"
+filename_base = "results//nominal//results"
+# filename_base = "results//uncertainty//results"
 congestions = [round(x, 1) for x in np.linspace(0.1, 0.9, 9)]
 congestions.extend([round(x, 1) for x in np.linspace(1.0, 2.0, 6)])
 # congestions = [0.1]
 
 schemes = {
-	# "naive": {"colour": "black"},
-	# "first": {"colour": "blue"},
-	# "cgs_cgr": {"colour": "red"},
-	# "cgs_cgr_resource": {"colour": "green"},
+	"naive": {"colour": "black"},
+	"first": {"colour": "blue"},
+	"cgs_cgr": {"colour": "red"},
+	"cgs_cgr_resource": {"colour": "green"},
 	"cgs_msr": {"colour": "orange"}
 }
 
 uncertainties = {
-	0.7: {"linestyle": "dotted"},
-	0.8: {"linestyle": "dashdot"},
-	0.9: {"linestyle": "dashed"},
+	# 0.7: {"linestyle": "dotted"},
+	# 0.8: {"linestyle": "dashdot"},
+	# 0.9: {"linestyle": "dashed"},
 	1.0: {"linestyle": "solid"}
 }
 
@@ -181,22 +180,22 @@ for metric in metrics:
 		for uncertainty in uncertainties:
 			metric[scheme][uncertainty] = []
 
-# first_pickup_ids = {}
-# for con in congestions:
-# 	# filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
-# 	filename = f"{filename_base}_first_{con}"
-# 	first_results = pickle.load(open(filename, "rb"))
-#
-# 	# Dict showing the time at which the pickup occurred (value) for each request (key)
-# 	first_pickup_ids[con] = {
-# 		b.task.requests[0].uid: b.created_at
-# 		for b in first_results.get_bundles_delivered_in_active_period()
-# 	}
+first_pickup_ids = {}
+for con in congestions:
+	# filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
+	filename = f"{filename_base}_first_{con}"
+	first_results = pickle.load(open(filename, "rb"))
+
+	# Dict showing the time at which the pickup occurred (value) for each request (key)
+	first_pickup_ids[con] = {
+		b.task.requests[0].uid: b.created_at
+		for b in first_results.get_bundles_delivered_in_active_period()
+	}
 
 # for scheme, con in itertools.product(schemes, congestions):
 for scheme, uncertainty, con in itertools.product(schemes, uncertainties, congestions):
-	filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
-	# filename = f"{filename_base}_{scheme}_{con}"
+	# filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
+	filename = f"{filename_base}_{scheme}_{con}"
 	results = pickle.load(open(filename, "rb"))
 
 	request_latency[scheme][uncertainty].append(mean(results.request_latencies) / 3600)
@@ -227,9 +226,9 @@ for scheme, uncertainty, con in itertools.product(schemes, uncertainties, conges
 	requests_failed["max"] = max(requests_failed["max"], requests_failed[scheme][uncertainty][-1])
 	requests_delivered["max"] = max(requests_delivered["max"], requests_delivered[scheme][uncertainty][-1])
 
-	# first_pu_frac[scheme][uncertainty].append(
-	# 	get_fraction_of_first_pickups(results, first_pickup_ids[con])
-	# )
-
-plot_performance_metrics(schemes, uncertainties, congestions, metrics)
-# plot_first_pickups(schemes, first_pu_frac, congestions)
+	first_pu_frac[scheme][uncertainty].append(
+		get_fraction_of_first_pickups(results, first_pickup_ids[con])
+	)
+#
+# plot_performance_metrics(schemes, uncertainties, congestions, metrics)
+plot_first_pickups(schemes, first_pu_frac, congestions)
