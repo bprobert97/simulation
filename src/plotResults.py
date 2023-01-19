c63a92e077cd30c32495c398049ff82e29eb1b11
Christopher Lowe <christopher.lowe@strath.ac.uk>
2023-01-19 06:54:54 +0000

    Includes the plotting for "first pickup" fraction
    This shows the fraction of pickups that were done using the "first" opportunity. Basically, how many pickups occurred at the same time as the equivalent requests in the "First" scheme. Needs extracting out to its own function as currently needs manual intervention

--- src/plotResults.py
+++ src/plotResults.py
@@ -9,6 +9,108 @@ from statistics import mean, stdev
 from misc import my_ceil
 
 
+def get_fraction_of_first_pickups(results, first_pickup_ids):
+	# This is the number of bundle acquisitions that differ from the "first" scheme
+	first_pickups_counter = 0
+	pickup_ids = {
+		b.task.requests[0].uid: b.created_at
+		for b in results.get_bundles_delivered_in_active_period()
+	}
+	for req, time_ in first_pickup_ids.items():
+		if req in pickup_ids and time_ == pickup_ids[req]:
+			first_pickups_counter += 1
+	return first_pickups_counter / len(pickup_ids)
+
+
+def plot_performance_metrics(schemes, uncertainties, congestions, metrics):
+	# plt.rcParams['axes.grid'] = True
+	plt.style.use('_mpl-gallery')
+	fig, ax = plt.subplots(3, 3)
+	plt.subplots_adjust(left=0.05, bottom=0.07, right=0.96, top=0.93)
+
+	for scheme, props in schemes.items():
+		for uncertainty, prop_un in uncertainties.items():
+			for metric in metrics:
+				ax[metric["row"], metric["col"]].plot(
+					congestions,
+					metric[scheme][uncertainty],
+					linewidth=2,
+					color=props["colour"],
+					linestyle=prop_un["linestyle"]
+				)
+
+				# Pick-up latency, from Request to Pickup
+				ax[metric["row"], metric["col"]].set(
+					xlim=(0, 2), xticks=np.arange(0, 2.5, 0.5),
+					ylim=(0, metric["max"]),
+					yticks=np.arange(0, metric["max"] + metric["tick"], metric["tick"])
+				)
+
+				ax[metric["row"], metric["col"]].set_ylabel(metric["y_label"])
+
+	ax[2, 0].set_xlabel("Congestion")
+	ax[2, 1].set_xlabel("Congestion")
+	ax[2, 2].set_xlabel("Congestion")
+
+	# Add a legend at the top of the figure
+	fig.legend(
+		ax[0, 0].lines,
+		# ["Naive", "First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
+		["Uncertainty = 0.7", "Uncertainty = 0.8", "Uncertainty = 0.9",
+		 "Uncertainty = 1.0"],
+		loc='upper center',
+		# ncol=len(schemes),
+		ncol=len(uncertainties)
+	)
+
+	# for a in ax.flat:
+	# 	a.label_outer()
+
+	plt.show()
+
+
+def plot_first_pickups(schemes, first_pickups, congestions):
+	plt.style.use('_mpl-gallery')
+	fig, ax = plt.subplots(1, 1)
+
+	for scheme, props in schemes.items():
+		ax.plot(
+			congestions,
+			first_pickups[scheme][1.0],
+			linewidth=2,
+			color=props["colour"],
+		)
+
+		ax.set(
+			xlim=(0, 2), xticks=np.arange(0, 2.5, 0.5),
+			ylim=(0, 1),
+			yticks=np.arange(0, 1.1, .1)
+		)
+
+		ax.set_ylabel("Fraction of first pickups")
+
+	ax.set_xlabel("Congestion")
+	plt.subplots_adjust(left=0.11, bottom=0.1, right=0.95, top=0.89)
+
+	# Add a legend at the top of the figure
+	fig.legend(
+		ax.lines,
+		["First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
+		# ["Uncertainty = 0.7", "Uncertainty = 0.8", "Uncertainty = 0.9",
+		#  "Uncertainty = 1.0"],
+		loc='upper center',
+		ncol=len(schemes),
+		# ncol=len(uncertainties)
+	)
+
+	# for a in ax.flat:
+	# 	a.label_outer()
+
+	plt.show()
+
+
+# filename_base = "results//nominal//results"
+filename_base = "results//uncertainty//results"
 congestions = [round(x, 1) for x in np.linspace(0.1, 0.9, 9)]
 congestions.extend([round(x, 1) for x in np.linspace(1.0, 2.0, 6)])
 # congestions = [0.1]
@@ -53,6 +155,12 @@ requests_failed = {
 requests_delivered = {
 	"row": 2, "col": 2, "y_label": "Req. delivered, x1000", "max": 1, "tick": 1}
 
+first_pu_frac = {
+	scheme: {
+		uncertainty: [] for uncertainty in uncertainties
+	} for scheme in schemes
+}
+
 metrics = [
 	request_latency,
 	task_latency,
@@ -73,9 +181,22 @@ for metric in metrics:
 		for uncertainty in uncertainties:
 			metric[scheme][uncertainty] = []
 
+# first_pickup_ids = {}
+# for con in congestions:
+# 	# filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
+# 	filename = f"{filename_base}_first_{con}"
+# 	first_results = pickle.load(open(filename, "rb"))
+#
+# 	# Dict showing the time at which the pickup occurred (value) for each request (key)
+# 	first_pickup_ids[con] = {
+# 		b.task.requests[0].uid: b.created_at
+# 		for b in first_results.get_bundles_delivered_in_active_period()
+# 	}
+
 # for scheme, con in itertools.product(schemes, congestions):
 for scheme, uncertainty, con in itertools.product(schemes, uncertainties, congestions):
-	filename = f"results//uncertainty//results_{scheme}_{uncertainty}_{con}"
+	filename = f"{filename_base}_{scheme}_{uncertainty}_{con}"
+	# filename = f"{filename_base}_{scheme}_{con}"
 	results = pickle.load(open(filename, "rb"))
 
 	request_latency[scheme][uncertainty].append(mean(results.request_latencies) / 3600)
@@ -106,46 +227,9 @@ for scheme, uncertainty, con in itertools.product(schemes, uncertainties, conges
 	requests_failed["max"] = max(requests_failed["max"], requests_failed[scheme][uncertainty][-1])
 	requests_delivered["max"] = max(requests_delivered["max"], requests_delivered[scheme][uncertainty][-1])
 
-# plt.rcParams['axes.grid'] = True
-plt.style.use('_mpl-gallery')
-fig, ax = plt.subplots(3, 3)
-plt.subplots_adjust(left=0.05, bottom=0.07, right=0.96, top=0.93)
+	# first_pu_frac[scheme][uncertainty].append(
+	# 	get_fraction_of_first_pickups(results, first_pickup_ids[con])
+	# )
 
-for scheme, props in schemes.items():
-	for uncertainty, prop_un in uncertainties.items():
-		for metric in metrics:
-			ax[metric["row"], metric["col"]].plot(
-				congestions,
-				metric[scheme][uncertainty],
-				linewidth=2,
-				color=props["colour"],
-				linestyle=prop_un["linestyle"]
-			)
-
-			# Pick-up latency, from Request to Pickup
-			ax[metric["row"], metric["col"]].set(
-				xlim=(0, 2), xticks=np.arange(0, 2.5, 0.5),
-				ylim=(0, metric["max"]),
-				yticks=np.arange(0, metric["max"] + metric["tick"], metric["tick"])
-			)
-
-			ax[metric["row"], metric["col"]].set_ylabel(metric["y_label"])
-
-ax[2, 0].set_xlabel("Congestion")
-ax[2, 1].set_xlabel("Congestion")
-ax[2, 2].set_xlabel("Congestion")
-
-# Add a legend at the top of the figure
-fig.legend(
-	ax[0, 0].lines,
-	# ["Naive", "First", "CGS (PU)", "CGS (CGR)", "CGS (MSR)"],
-	[0.7, 0.8, 0.9, 1.0],
-	loc='upper center',
-	# ncol=len(schemes),
-	ncol=len(uncertainties)
-)
-
-# for a in ax.flat:
-# 	a.label_outer()
-
-plt.show()
+plot_performance_metrics(schemes, uncertainties, congestions, metrics)
+# plot_first_pickups(schemes, first_pu_frac, congestions)
