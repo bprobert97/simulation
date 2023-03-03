a19e13d16b363a3a32328af96b5e81d3dc19788f
Christopher Lowe <christopher.lowe@strath.ac.uk>
2023-03-03 16:08:07 +0000

    Prep for full analysis at 0.7

--- src/mainMulti.py
+++ src/mainMulti.py
@@ -10,21 +10,22 @@ import misc as _misc
 
 # [valid_pickup, define_pickup, valid_delivery, resource_aware, define_delivery]
 schemes = {
-	# "naive":              [False, False, False, False, False],
-	# "first":              [True,  True,  False, False, False],
-	# "cgs_cgr":            [True,  True,  True,  False, False],
-	"cgs_cgr_resource":   [True,  True,  True,  True,  False],
+	"naive":              [False, False, False, False, False],
+	"first":              [True,  True,  False, False, False],
+	"cgs_cgr":            [True,  True,  True,  False, False],
+	# "cgs_cgr_resource":   [True,  True,  True,  True,  False],
 	# "cgs_msr":            [True,  True,  True,  True,  True],
 }
 
-uncertainties = [1.0, 0.9, 0.8, 0.7]
+# uncertainties = [1.0, 0.9, 0.8, 0.7]
+uncertainties = [0.7]
 
 congestions = [.1, .2, .3, .4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.]
 # congestions = np.linspace(0.1, 0.9, 3)
 # congestions = [0.2, 0.5, 1.0]
 
 filename = "input_files//walker_delta_16.json"
-results_file_base = "results//uncertainty//cgs_cgr//results"
+results_file_base = "results//uncertainty//results"
 with open(filename, "rb") as read_content:
 	inputs = json.load(read_content, object_hook=lambda d: SimpleNamespace(**d))
 
